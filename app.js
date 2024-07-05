2c2893db8449decb54c0d5fbaf8d3ee09a437949
Duaa Saleem <157964862+duaa-khann@users.noreply.github.com>
2024-07-05 21:38:24 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+
+// function addNumbers(a, b) {
+//    return a + b;
+// }
+
+// let num1 = 5;
+// let num2 = 10;
+
+// let result1 = addNumbers(num1, num2);
+// console.log(`Result of adding ${num1} and ${num2}:`, result1);
+
+// let num3 = 7;
+// let num4 = 3;
+// let result2 = addNumbers(num3, num4);
+// console.log(`Result of adding ${num3} and ${num4}:`, result2);
+
+
+
+// const descriptiveWords = ['awesome', 'fantastic', 'amazing', 'brilliant', 'wonderful'];
+
+// function greetWithRandomWord() {
+
+// let userName = prompt("What is your name?");
+
+//  let randomIndex = Math.floor(Math.random() * descriptiveWords.length);
+// let randomWord = descriptiveWords[randomIndex];
+
+
+// console.log(`Hello, ${userName}! You are ${randomWord}.`);
+// }
+
+// greetWithRandomWord();
+
+
+
+// let num1 = 10;
+// let num2 = 5;
+
+// let operator = '+';
+
+
+// function calculate(a, b, op) {
+// if (op === '+') {
+// return a + b;
+// } else if (op === '-') {
+// return a - b;
+// } else {
+// console.log("Invalid operator, defaulting to addition.");
+// return a + b;
+// }
+// }
+
+// console.log(`Result of ${num1} ${operator} ${num2}:`, calculate(num1, num2, operator));
+
+
+// operator = '-';
+
+
+// console.log(`Result of ${num1} ${operator} ${num2}:`, calculate(num1, num2, operator));
